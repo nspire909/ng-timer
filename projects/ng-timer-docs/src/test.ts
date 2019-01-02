ebcd17d696f635df0ebe61e83d496137c173d864
Nick Marks <nick.marks@eagletg.com>
2019-01-02 16:53:28 -0600

    ci: added review

--- projects/ng-timer-docs/src/test.ts
+++ projects/ng-timer-docs/src/test.ts
@@ -14,6 +14,19 @@ getTestBed().initTestEnvironment(
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting()
 );
+
+// code coverage only covers files that are loaded.  so when
+//  running code coverage, load all modules.
+// Ideally we'd do something more like this:
+//  https://github.com/gotwarlost/istanbul/issues/112#issuecomment-221851973
+//  but that's a lot more setup
+declare const __karma__: any;
+if (__karma__.config.codeCoverage) {
+  // For Angular 5.x, we had to add "**/*.module.ts" to the include array in tsconfig.spec.json
+  const reqContext = require.context('./', true, /\.module\.ts$/);
+  reqContext.keys().map(reqContext);
+}
+
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
